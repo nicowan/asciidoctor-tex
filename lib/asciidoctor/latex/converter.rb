7ed35a946cd2af8959d2a2226360dd866775e9e4
Nicolas Wanner <nicoocin.info@gmail.com>
2020-05-20 22:39:38 +0200

    cleanup for Document, Section, Paragraph and InlineQuoted

--- lib/asciidoctor/latex/converter.rb
+++ lib/asciidoctor/latex/converter.rb
@@ -2,120 +2,137 @@ require 'asciidoctor'
 require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
 require 'asciidoctor/latex/core_ext/colored_string'
 require 'asciidoctor/latex/ent_to_uni'
-require 'asciidoctor/latex/environment_block'
 require 'asciidoctor/latex/node_processors'
-require 'asciidoctor/latex/prepend_processor'
-require 'asciidoctor/latex/macro_insert'
 require 'asciidoctor/latex/tex_block'
 require 'asciidoctor/latex/tex_preprocessor'
-require 'asciidoctor/latex/macro_preprocessor'
-require 'asciidoctor/latex/dollar'
 require 'asciidoctor/latex/tex_postprocessor'
-require 'asciidoctor/latex/sectnumoffset-treeprocessor'
 
 $VERBOSE = true
 
-module Asciidoctor::Latex
+module Asciidoctor
+  module Latex
 
-  class LatexEscape < Asciidoctor::Extensions::Preprocessor 
-    def process document, reader
-      return reader if reader.eof?
-      replacement_lines = reader.read_lines.map do |line|
-        #line = line.gsub('{', 'LATEXOPENBRACE')                #    "\\\\{")
-        #line = line.gsub('}', 'LATEXCLOSEBRACE')               #    "\\\\}")
-        #line = line.gsub("&", 'LATEXAMPERSAND')                #    "\\\\&")
-        #line = line.gsub("#", 'LATEXSHARP')                    #    "\\\\#")
-        #line = line.gsub("%", 'LATEXPERCENT')                  #    "\\%")
-        #line = line.gsub("$", 'LATEXDOLLAR')                   #    "\\$")
-        #line = line.gsub("_", 'LATEXUNDERSCORE')               #    "\\_")
-        #line = line.gsub("|", 'LATEXVERTICALPIPE')             #    "\\textbar{}")
-        #line = line.gsub("~", 'LATEXTILDE')                    #    "\\textasciitilde{}")
-        #line = line.gsub("^", 'LATEXCIRCONFLEXE')              #    "\\textasciicircum{}")
-        line
-      end
-      reader.unshift_lines replacement_lines
-      reader
-    end
-  end
-
-  class LatexUnescape < Asciidoctor::Extensions::Postprocessor 
-    def process document, output
-      # When writing latex commands in the target document,
-      # I use some improbable UNICODE chars for curly and square braces
-      # And just before giving back the converted document I convert it 
-      # to the right LaTeX characters
-      output = output.gsub('《', "{")
-      output = output.gsub('》', "}")
-      output = output.gsub('〈', "[")
-      output = output.gsub('〉', "]")
-    end
-  end
-
-  class Converter
-    include Asciidoctor::Converter
-    register_for 'latex'
-
-
-    # puts "HOLA!".red
-
-    Asciidoctor::Extensions.register do
-      postprocessor EntToUni if document.basebackend? 'tex' unless document.attributes['unicode'] == 'no'
-      preprocessor  LatexEscape
-      postprocessor LatexUnescape
-
-    end
-
-    def initialize backend, opts
-      super
-      basebackend    'tex'
-      outfilesuffix '.tex'
-    end
-
-    # FIXME: find a solution without a global variable
-    $latex_environment_names = []
-
-    def convert node, transform = nil
-      case node.node_name
-      when 'document';            node.tex_process  # OK
-      when 'section';             node.tex_process  # OK
-      when 'dlist';               node.tex_process  # OK
-      when 'olist';               node.tex_process  # OK
-      when 'ulist';               node.tex_process  # OK
-      when 'inline_anchor';       node.tex_process  # 
-      when 'inline_break';        node.tex_process  # 
-      when 'inline_footnote';     node.tex_process  # 
-      when 'inline_quoted';       node.tex_process  # 
-      when 'inline_callout';      node.tex_process  # 
-      when 'inline_indexterm';    node.tex_process  # 
-      when 'admonition';          node.tex_process  # OK
-      when 'listing';             node.tex_process  # 
-      when 'literal';             node.tex_process  # 
-      when 'page_break';          node.tex_process  # 
-      when 'paragraph';           node.tex_process  # 
-      when 'stem';                node.tex_process  # 
-      when 'pass';                node.tex_process  # 
-      when 'open';                node.tex_process  # 
-      when 'quote';               node.tex_process  # 
-      when 'example';             node.tex_process  # 
-      when 'floating_title';      node.tex_process  # 
-      when 'image';               node.tex_process  # 
-      when 'preamble';            node.tex_process  # 
-      when 'sidebar';             node.tex_process  # 
-      when 'verse';               node.tex_process  # 
-      when 'toc';                 node.tex_process  # 
-      when 'table';               node.tex_process  # 
-      when 'thematic_break';      warn "#{node.node_name} is not implemented"
-      when 'colist';              warn "#{node.node_name} is not implemented"
-      when 'embedded';            warn "#{node.node_name} is not implemented"
-      when 'inline_button';       warn "#{node.node_name} is not implemented"
-      when 'inline_image';        node.tex_process
-      when 'inline_kbd';          warn "#{node.node_name} is not implemented"
-      when 'inline_menu';         warn "#{node.node_name} is not implemented"
-      when 'video';               warn "#{node.node_name} is not implemented"
-      when 'audio';               warn "#{node.node_name} is not implemented"
-      else
-        warn %(Node to implement: #{node.node_name}, class = #{node.class}).magenta #if $VERBOSE
+    class LatexEscape < Asciidoctor::Extensions::Preprocessor 
+      def process document, reader
+        return reader if reader.eof?
+        replacement_lines = reader.read_lines.map do |line|
+          #line = line.gsub('{', 'LATEXOPENBRACE')                #    "\\\\{")
+          #line = line.gsub('}', 'LATEXCLOSEBRACE')               #    "\\\\}")
+          #line = line.gsub("&", 'LATEXAMPERSAND')                #    "\\\\&")
+          #line = line.gsub("#", 'LATEXSHARP')                    #    "\\\\#")
+          #line = line.gsub("%", 'LATEXPERCENT')                  #    "\\%")
+          #line = line.gsub("$", 'LATEXDOLLAR')                   #    "\\$")
+          #line = line.gsub("_", 'LATEXUNDERSCORE')               #    "\\_")
+          #line = line.gsub("|", 'LATEXVERTICALPIPE')             #    "\\textbar{}")
+          #line = line.gsub("~", 'LATEXTILDE')                    #    "\\textasciitilde{}")
+          #line = line.gsub("^", 'LATEXCIRCONFLEXE')              #    "\\textasciicircum{}")
+          line
+        end
+        reader.unshift_lines replacement_lines
+        reader
       end
     end
-  end # class Converter
-end # module Asciidoctor::Latex
+
+    class LatexUnescape < Asciidoctor::Extensions::Postprocessor 
+      def process document, output
+        # When writing latex commands in the target document,
+        # I use some improbable UNICODE chars for curly and square braces
+        # And just before giving back the converted document I convert it 
+        # to the right LaTeX characters
+        output = output.gsub('《', "{")
+        output = output.gsub('》', "}")
+        output = output.gsub('〈', "[")
+        output = output.gsub('〉', "]")
+      end
+    end
+
+    class Converter
+      include Asciidoctor::Converter
+      include Process
+      register_for 'latex'
+
+      # puts "HOLA!".red
+
+      Asciidoctor::Extensions.register do
+        postprocessor EntToUni if document.basebackend? 'tex' unless document.attributes['unicode'] == 'no'
+        preprocessor  LatexEscape
+        postprocessor LatexUnescape
+
+      end
+
+      def initialize backend, opts
+        super
+        basebackend    'tex'
+        outfilesuffix '.tex'
+      end
+
+      def convert node, transform = nil
+
+        #if defined?(node.type)
+        #  if defined?(node.text)
+        #    puts "#{node.node_name} / #{node.type} = #{node.text}"
+        #  else
+        #    puts "#{node.node_name} / #{node.type} = no text"
+        #  end
+        #else
+        #  if defined?(node.text)
+        #    puts "#{node.node_name} = #{node.text}"
+        #  else
+        #    puts "#{node.node_name} = no text"
+        #  end
+        #end
+
+        case node.node_name
+          # Block ------------------------------------------------------
+          when 'document';            Process.document(node)
+          when 'section';             Process.section(node)
+          when 'paragraph';           Process.paragraph(node)
+          when 'listing';             Process.listing(node)
+
+          # Inline -----------------------------------------------------
+          when 'inline_quoted';       Process.inlineQuoted(node)
+
+
+        when 'dlist';               node.tex_process  # OK
+        when 'olist';               node.tex_process  # OK
+        when 'ulist';               node.tex_process  # OK
+
+        when 'inline_anchor';       node.tex_process  # 
+        when 'inline_break';        node.tex_process  # 
+        when 'inline_footnote';     node.tex_process  # 
+        when 'inline_callout';      node.tex_process  # 
+        when 'inline_indexterm';    node.tex_process  # 
+        when 'admonition';          node.tex_process  # OK
+        when 'literal';             node.tex_process  # 
+        when 'page_break';          node.tex_process  # 
+        when 'stem';                node.tex_process  # 
+        when 'pass';                node.tex_process  # 
+        when 'open';                node.tex_process  # 
+        when 'quote';               node.tex_process  # 
+        when 'example';             node.tex_process  # 
+        when 'floating_title';      node.tex_process  # 
+        when 'image';               node.tex_process  # 
+        when 'preamble';            node.tex_process  # 
+        when 'sidebar';             node.tex_process  # 
+        when 'verse';               node.tex_process  # 
+        when 'toc';                 node.tex_process  # 
+        when 'table';               node.tex_process  # 
+        when 'thematic_break';      warn "#{node.node_name} is not implemented"
+        when 'colist';              warn "#{node.node_name} is not implemented"
+        when 'embedded';            warn "#{node.node_name} is not implemented"
+        when 'inline_button';       warn "#{node.node_name} is not implemented"
+        when 'inline_image';        node.tex_process
+        when 'inline_kbd';          warn "#{node.node_name} is not implemented"
+        when 'inline_menu';         warn "#{node.node_name} is not implemented"
+        when 'video';               warn "#{node.node_name} is not implemented"
+        when 'audio';               warn "#{node.node_name} is not implemented"
+        else
+          warn %(Node to implement: #{node.node_name}, class = #{node.class}).magenta #if $VERBOSE
+        end
+      end
+    end # class Converter
+
+
+
+  end # module Latex
+end # module Asciidoctor
